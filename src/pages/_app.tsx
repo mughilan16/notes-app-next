05b9ff492bbb3c95157324a2f471eba8c7383ce3
mughilan16 <mughilant@proton.me>
2023-07-26 15:50:25 +0530

    space

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Head from "next/head";
 
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
